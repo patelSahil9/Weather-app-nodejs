447596904574038ff751ba6739955b51c83b9966
patelSahil9 <ptulasi578@gmail.com>
2025-02-01 10:35:06 +0530

    .

--- src/app.js
+++ src/app.js
@@ -1,28 +1,40 @@
-const express = require('express');
-const hbs = require('hbs');
-const path = require('path');
-
+const express = require("express");
+const weatherData = require("../utils/weatherData");
+const path = require("path");
+const hbs = require("hbs");
 const app = express();
-const weatherData = require('../utils/weatherData');
-
+const publicPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+app.use(express.static(publicPath));
 const port = process.env.PORT || 3000;
-
-
-app.get('/weather', (req, res) => {
-    // res.send('Weather');
-    {
-        if (!req.query.address) {
-            return res.send("Address is required");
-        }
-        weatherData(req.query.address, (error, result) => {
-            if (error) {
-                return res.send(error);
-            }
-            res.send(result);
-        });
-    } 
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
+// Route to retrieve weather data for a given address
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({ error: "Address is required" });
+  }
+
+  weatherData(req.query.address, (error, data) => {
+    if (error) {
+      return res.send({ error });
+    }
+    res.send(data);
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404",{
+    title: "Page not found",});
+});
+
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
--- utils/weatherData.js
+++ utils/weatherData.js
@@ -1,21 +1,23 @@
 const request = require('request');
 
-//API key = bdbffb3dd8d6c3451dff4c60cb39ff46
-
 const openweathermap = {
     BASE_URL: 'https://api.openweathermap.org/data/2.5/weather',
-    SECRET_KEY: 'e9543918b5894c94e4485dfca4f0a7ff'
-}
+    SECRET_KEY: 'bdbffb3dd8d6c3451dff4c60cb39ff46',
+};
 
 const weatherData = (address, callback) => {
-    const url = openweathermap.BASE_URL + encodeURIComponent(address) + '&appid=' + openweathermap.SECRET_KEY;
+    const url = `${openweathermap.BASE_URL}?q=${encodeURIComponent(address)}&appid=${openweathermap.SECRET_KEY}`;
 
-    request({ url, json: true }, (error,data) => {
-        if(error) {
+    request({ url, json: true }, (error, { body } = {}) => {
+        if (error) {
             callback('Unable to connect to weather service', undefined);
-        } 
-        callback(false,data?.body);
+        } else if (body.cod !== 200) {
+            callback(body.message, undefined);
+        } else {
+            callback(false, body); // Send the weather data
+        }
     });
-}
+};
 
 module.exports = weatherData;
+    
