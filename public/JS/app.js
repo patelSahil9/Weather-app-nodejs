89a5a79705a7a3240fca5c1706a58440845d9500
patelSahil9 <ptulasi578@gmail.com>
2025-02-03 18:23:45 +0530

    done

--- public/JS/app.js
+++ public/JS/app.js
@@ -25,26 +25,68 @@ weatherForm.addEventListener("submit", (e) => {
   tempElement.textContent = "";
   weatherCondition.textContent = "";
 
+  showData(search.value);
 });
-function showData(city){
-    getWeatherData(city,(result) => {
-        // console.log(result);
-        if (result.cod === "200") {
-            weatherIcon.className = "wi wi-day-cloudy";
-            locationElement.textContent = result?.name;
-            tempElement.textContent = result?.main?.temp;
-        }
-        else {
-            locationElement.textContent ="city not found";
-        }
-    })
+
+if ("geolocation" in navigator) {
+  locationElement.textContent = "Loading...";
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      const apiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
+
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data && data.address && data.address.city) {
+            const city = data.address.city;
+
+            showData(city);
+          } else {
+            console.error("City not found in location data.");
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching location data:", error);
+        });
+    },
+    function (error) {
+      console.error("Error getting location:", error.message);
+    }
+  );
+} else {
+  console.error("Geolocation is not available in this browser.");
 }
 
-function getWeatherData(city,callback) {
-    const locationApi = `/weather?address=${city}`;
-    fetch(locationApi).then((response) => {
-        response.json().then((data) => {
-            callback(response);
-        });
+function showData(city) {
+  getWeatherData(city, (result) => {
+    console.log(result);
+    if (result.cod == 200) {
+      if (
+        result.weather[0].description == "rain" ||
+        result.weather[0].description == "fog"
+      ) {
+        weatherIcon.className = "wi wi-day-" + result.description;
+      } else {
+        weatherIcon.className = "wi wi-day-cloudy";
+      }
+      locationElement.textContent = result?.name;
+      tempElement.textContent =
+        (result?.main?.temp - 273.5).toFixed(2) + String.fromCharCode(176);
+      weatherCondition.textContent =
+        result?.weather[0]?.description?.toUpperCase();
+    } else {
+      locationElement.textContent = "City not found.";
+    }
+  });
+}
+
+function getWeatherData(city, callback) {
+  const locationApi = weatherApi + "?address=" + city;
+  fetch(locationApi).then((response) => {
+    response.json().then((response) => {
+      callback(response);
     });
+  });
 }
